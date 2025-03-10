cbcafe0013c6a49e15d65f91c25c56d97ac840c2
Vishal-Ravan <vksingh2992000@gmail.com>
2025-03-10 15:24:59 +0530

    content change about us page

--- src/components/about/About.svelte
+++ src/components/about/About.svelte
@@ -1,13 +1,35 @@
-<div class=" mx-auto max-w-screen-xl px-4 py-22 lg:flex lg:items-center">
+<div class=" py-22 mx-auto max-w-screen-xl px-4 lg:flex lg:items-center">
 	<div class="grid grid-cols-1 gap-14 md:grid-cols-2">
 		<!-- Who are we? -->
 		<div>
 			<h2 class="text-xl font-bold text-green-700 md:text-2xl">Who are we?</h2>
 			<p class="mt-4 text-gray-700">
-				We are a coalition comprising top industry experts who have joined forces to ensure that
-				India’s path towards developing the National Emissions Trading System (ETS) and a robust
-				Domestic Carbon Market is well-structured. Our primary goal is to provide the necessary
-				policy advocacy and regulatory framework to support this endeavor.
+				The Carbon Markets Association of India (CMAI) is a leading not-for-profit group of industry
+				stakeholders dedicated to accelerating India’s transition to a sustainable, net-zero future.
+				As a strategic alliance of industry experts, policymakers, and global stakeholders, CMAI
+				aims to develop a robust carbon credit market and drive India’s climate and sustainability
+				goals
+				<br />
+				<br />
+				CMAI collaborates closely with key ministries such as ICVCM, India’s MoEFCC, MoP, MNRE, and NITI
+				Aayog, providing policy advocacy and knowledge support. It plays a pivotal role in initiatives
+				like the Carbon Credit Trading Scheme (CCTS) and the Green Credit Programme (GCP), ensuring alignment
+				with global standards and promoting climate justice.
+				<br />
+				<br />
+
+				We equip stakeholders through capacity-building programs, training workshops, and grassroots
+				campaigns. With members like Indian Oil, Tata Power, Reliance, and more, CMAI integrates
+				carbon market mechanisms into sectors like agriculture and renewable energy while fostering
+				international collaborations for sustainable solutions.
+				<br />
+				<br />
+
+				CMAI has supported over 3,500 projects, reducing 220 million tons of GHG emissions and
+				actively participates in global climate negotiations. Beyond carbon markets, CMAI champions
+				innovations like the India Clean Cooking Alliance, India EPR Alliance, Centre of Excellence
+				ANT initiative, and biodiversity conservation among others, advancing solutions that
+				harmonize economic growth and environmental stewardship.
 			</p>
 		</div>
 
@@ -26,21 +48,25 @@
 </div>
 <div class="mx-auto max-w-screen-xl px-4 py-12 lg:flex lg:items-center">
 	<div
-		class="relative flex flex-col items-center overflow-hidden rounded-4xl bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
+		class="rounded-4xl relative flex flex-col items-center overflow-hidden bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
 	>
 		<div
 			class="absolute inset-0 z-0 bg-black opacity-50 transition-opacity duration-300 group-hover:opacity-50"
 		></div>
 
 		<div
-			class="z-10 flex h-100 flex-col justify-center rounded-4xl rounded-4xl border-6 border-double border-gray-300 bg-slate-800 p-6 p-6 text-white text-white md:w-1/1 md:w-1/2 md:p-10 md:p-10"
+			class="h-130 rounded-4xl rounded-4xl border-6 md:w-1/1 z-10 flex flex-col justify-center border-double border-gray-300 bg-slate-800 p-6 p-6 text-white text-white md:w-1/2 md:p-10 md:p-10"
 		>
 			<h2 class="text-2xl font-bold md:text-3xl">Our Vision</h2>
 			<p class="mt-4 text-gray-200">
-				We envision that the country has become net zero with the help of a robust domestic credit
-				market that has been built unanimously in deep cosultation with the best industry experts.
-				We want to develop a resilient market that is inclusive yet dymanic so that India’s quest to
-				a net-zero future can be accomiplished sooner than anticipated.
+				CMAI envisions a world where inclusive, dynamic, and sustainable pathways drive
+				decarbonization and global climate resilience. We look forward to positioning India as a
+				global leader in carbon markets, fostering sustainable growth, equitable climate solutions,
+				and innovation-driven practices for a net-zero future. We look to the crucial role of the
+				domestic carbon market in making India carbon neutral by focusing on measurable
+				contributions toward India's interim climate targets (e.g., Nationally Determined
+				Contributions - NDCs). We envision policies and frameworks that ensure environmental
+				integrity, economic efficiency, and social inclusion.
 			</p>
 		</div>
 
@@ -50,38 +76,44 @@
 
 <div class="mx-auto max-w-screen-xl px-4 py-12 lg:flex lg:items-center">
 	<div
-		class="relative flex flex-col items-center overflow-hidden rounded-4xl bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
+		class="rounded-4xl relative flex flex-col items-center overflow-hidden bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
 	>
 		<div
 			class="absolute inset-0 z-0 bg-black opacity-50 transition-opacity duration-300 group-hover:opacity-50"
 		></div>
 
 		<div
-			class="z-10 flex h-100 flex-col justify-center rounded-4xl rounded-4xl border-6 border-double border-gray-300 bg-slate-800 p-6 p-6  text-white md:w-1/1 md:w-1/2 md:p-10 md:p-10"
+			class="h-130 rounded-4xl rounded-4xl border-6 md:w-1/1 z-10 flex flex-col justify-center border-double border-gray-300 bg-slate-800 p-6 p-6 text-white md:w-1/2 md:p-10 md:p-10"
 		>
 			<h2 class="text-2xl font-bold md:text-3xl">Our Mission</h2>
 			<p class="mt-4 text-gray-200">
-				Our mission is to empower the market with our advisory support and innovative solutions. We
-				are committed to bringing together the entire industry to achieve a shared goal of emission
-				reduction by formulating a framework for the carbon market.
+				To empower businesses, communities, and governments by uniting stakeholders and providing
+				innovative solutions for emissions reduction, carbon offset management, and climate
+				resilience by leveraging carbon markets. We are also on a mission to being all
+				environment-friendly practices, professionals, and businesses like extended producer
+				responsibility or climate law professionals on one single platform for an efficient and
+				smooth transition of India to net zero through policy advocacy.
 			</p>
 		</div>
 
 		<div class="md:w-1/1">
-        <h2 class="text-4xl text-white font-bold md:text-4xl text-center z-10 relative">A carbon neutral India</h2></div>
+			<h2 class="relative z-10 text-center text-4xl font-bold text-white md:text-4xl">
+				A carbon neutral India
+			</h2>
+		</div>
 	</div>
 </div>
 
 <div class="mx-auto max-w-screen-xl px-4 py-12 lg:flex lg:items-center">
 	<div
-		class="relative flex flex-col items-center overflow-hidden rounded-4xl bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
+		class="rounded-4xl relative flex flex-col items-center overflow-hidden bg-[url('/images/banner.jpg')] bg-cover bg-center shadow-lg md:flex-row"
 	>
 		<div
 			class="absolute inset-0 z-0 bg-black opacity-50 transition-opacity duration-300 group-hover:opacity-50"
 		></div>
 
 		<div
-			class="z-10 flex h-100 flex-col justify-center rounded-4xl rounded-4xl border-6 border-double border-gray-300 bg-slate-800 p-6 p-6 text-white text-white md:w-1/1 md:w-1/2 md:p-10 md:p-10"
+			class="h-130 rounded-4xl rounded-4xl border-6 md:w-1/1 z-10 flex flex-col justify-center border-double border-gray-300 bg-slate-800 p-6 p-6 text-white text-white md:w-1/2 md:p-10 md:p-10"
 		>
 			<h2 class="text-2xl font-bold md:text-3xl">The Background</h2>
 			<p class="mt-4 text-gray-200">
@@ -94,8 +126,9 @@
 		</div>
 
 		<div class="md:w-1/1">
-        <h2 class="text-4xl text-white font-bold md:text-4xl text-center z-10 relative">Building a sustainable carbon market ecosystem</h2>
-
-        </div>
+			<h2 class="relative z-10 text-center text-4xl font-bold text-white md:text-4xl">
+				Building a sustainable carbon <br/> market ecosystem
+			</h2>
+		</div>
 	</div>
 </div>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,4 @@
 <script>
-
 	let showAboutDropdown = false;
 	let showOfferingsDropdown = false;
 	let showClimateDropdown = false;
@@ -22,6 +21,7 @@
 			name: 'About Us',
 			key: 'about',
 			links: [
+				{ path: '/about-us', label: 'About US' },
 				{ path: '/our-team', label: 'Our Team' },
 				{ path: '/members', label: 'Members' }
 			]
@@ -62,7 +62,7 @@
 	<div class=" hidden lg:block">
 		<div class="mx-auto flex max-w-screen-xl items-center gap-2 px-6 pb-2">
 			<div class="flex size-full h-16 w-full max-w-screen-xl items-center gap-2">
-				<a class="block flex gap-2 text-sm text-white" href="#">
+				<a class="block flex gap-2 text-sm text-white" href="mailto:secretary@cma-india.in">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
@@ -75,7 +75,7 @@
 						/>
 					</svg> secretary@cma-india.in
 				</a>
-				<a class="block flex gap-2 text-sm text-white" href="#">
+				<a class="block flex gap-2 text-sm text-white" href="https://maps.app.goo.gl/jvy6GRmjSP45weN96" target="_blank">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
@@ -87,7 +87,7 @@
 							d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"
 						/>
 					</svg>
-					Working Office: 511, World Trade Centre, Connaught Place, New Delhi - 110001
+					Working Office: 511, World Trade Centre, Barakhamba Road, New Delhi- 110001
 				</a>
 			</div>
 
@@ -103,6 +103,7 @@
 						d="M12 2.2c3.2 0 3.6 0 4.9.1 1.2.1 2 .3 2.7.7.7.3 1.3.8 1.9 1.4.5.6 1 1.2 1.4 1.9.4.7.6 1.5.7 2.7.1 1.3.1 1.7.1 4.9s0 3.6-.1 4.9c-.1 1.2-.3 2-.7 2.7-.3.7-.8 1.3-1.4 1.9-.6.5-1.2 1-1.9 1.4-.7.4-1.5.6-2.7.7-1.3.1-1.7.1-4.9.1s-3.6 0-4.9-.1c-1.2-.1-2-.3-2.7-.7-.7-.3-1.3-.8-1.9-1.4-.5-.6-1-1.2-1.4-1.9-.4-.7-.6-1.5-.7-2.7C2.2 15.6 2.2 15.2 2.2 12s0-3.6.1-4.9c.1-1.2.3-2 .7-2.7.3-.7.8-1.3 1.4-1.9.6-.5 1.2-1 1.9-1.4.7-.4 1.5-.6 2.7-.7C8.4 2.2 8.8 2.2 12 2.2m0-2.2C8.7 0 8.3 0 7 .1 5.7.2 4.6.5 3.7 1 .9 1.8 0 3.1 0 5c-.1 1.2-.1 1.6-.1 4.9s0 3.6.1 4.9c.1 1.9.9 3.2 2.7 4 .9.5 2 .8 3.3.9 1.3.1 1.7.1 4.9.1s3.6 0 4.9-.1c1.3-.1 2.4-.4 3.3-.9 1.8-.8 2.6-2.1 2.7-4 .1-1.2.1-1.6.1-4.9s0-3.6-.1-4.9c-.1-1.9-.9-3.2-2.7-4-.9-.5-2-.8-3.3-.9-1.3-.1-1.7-.1-4.9-.1zM12 5.7a6.3 6.3 0 110 12.6 6.3 6.3 0 010-12.6m0 10.4a4.1 4.1 0 100-8.2 4.1 4.1 0 000 8.2m7.3-10.9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
 					/>
 				</svg>
+				<a href="https://twitter.com/official_cmai" target="_blank">
 
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -115,7 +116,7 @@
 						d="M23.64 4.56c-.84.37-1.75.61-2.7.72a4.72 4.72 0 002.08-2.59 9.36 9.36 0 01-2.97 1.13 4.67 4.67 0 00-8.01 4.26A13.28 13.28 0 011.67 3.14a4.64 4.64 0 001.45 6.22c-.74-.02-1.46-.23-2.1-.57v.06a4.66 4.66 0 003.73 4.57 4.68 4.68 0 01-2.1.08 4.67 4.67 0 004.36 3.24A9.36 9.36 0 010 20.48a13.2 13.2 0 007.18 2.1c8.6 0 13.3-7.13 13.3-13.3l-.01-.6a9.5 9.5 0 002.35-2.42z"
 					/>
 				</svg>
-
+</a>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -127,19 +128,19 @@
 						d="M23.5 6.8c-.3-1.3-1.3-2.3-2.6-2.6C18.4 3.5 12 3.5 12 3.5s-6.4 0-8.9.7A3.2 3.2 0 00.5 6.8 32.1 32.1 0 000 12c0 1.7.1 3.4.5 5.2.3 1.3 1.3 2.3 2.6 2.6 2.5.7 8.9.7 8.9.7s6.4 0 8.9-.7a3.2 3.2 0 002.6-2.6c.3-1.8.5-3.5.5-5.2s-.1-3.4-.5-5.2zm-14.3 8.6V8.6l6.2 3.4-6.2 3.4z"
 					/>
 				</svg>
-
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-				>
-					<path
-						d="M22.23 0H1.77C.79 0 0 .77 0 1.72v20.56c0 .95.79 1.72 1.77 1.72h20.46c.98 0 1.77-.77 1.77-1.72V1.72C24 .77 23.21 0 22.23 0zM7.12 20.49H3.56V9h3.56v11.49zM5.34 7.56a2.06 2.06 0 110-4.13 2.06 2.06 0 010 4.13zm14.14 12.93h-3.56v-5.58c0-1.33-.03-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.95v5.67H8.37V9h3.42v1.57h.05c.48-.9 1.65-1.85 3.4-1.85 3.63 0 4.3 2.4 4.3 5.52v6.25z"
-					/>
-				</svg>
-
+				<a href="https://www.linkedin.com/company/86816772/admin/feed/posts/" target="_blank">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+					>
+						<path
+							d="M22.23 0H1.77C.79 0 0 .77 0 1.72v20.56c0 .95.79 1.72 1.77 1.72h20.46c.98 0 1.77-.77 1.77-1.72V1.72C24 .77 23.21 0 22.23 0zM7.12 20.49H3.56V9h3.56v11.49zM5.34 7.56a2.06 2.06 0 110-4.13 2.06 2.06 0 010 4.13zm14.14 12.93h-3.56v-5.58c0-1.33-.03-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.95v5.67H8.37V9h3.42v1.57h.05c.48-.9 1.65-1.85 3.4-1.85 3.63 0 4.3 2.4 4.3 5.52v6.25z"
+						/>
+					</svg>
+				</a>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -170,7 +171,7 @@
 						>
 						{#if (menu.key === 'about' && showAboutDropdown) || (menu.key === 'offerings' && showOfferingsDropdown) || (menu.key === 'climate' && showClimateDropdown) || (menu.key === 'knowledge' && showKnowledgeDropdown)}
 							<ul
-								class="absolute left-0 mt-2 w-2xs w-48 cursor-pointer rounded-md bg-white text-gray-900 shadow-lg"
+								class="w-2xs absolute left-0 mt-2 w-48 cursor-pointer rounded-md bg-white text-gray-900 shadow-lg"
 							>
 								{#each menu.links as link}
 									<li class="cursor-pointer">
