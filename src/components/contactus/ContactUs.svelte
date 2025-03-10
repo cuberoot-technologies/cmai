7abb40bb57017be541818183deef754a54cf94e7
Vishal-Ravan <vksingh2992000@gmail.com>
2025-03-10 12:54:47 +0530

    add conatc pages

---
+++ src/components/contactus/ContactUs.svelte
@@ -0,0 +1,75 @@
+<section class="mx-auto grid max-w-6xl items-center gap-8 py-22 md:grid-cols-2">
+	<div>
+		<h2 class="text-sm font-bold text-green-600 uppercase">Contact Us</h2>
+		<h1 class=" text-2xl font-bold text-sky-800 sm:text-5xl">Feel free to contact & reach us !!</h1>
+
+		<div class="mt-6 space-y-6">
+			<div class="flex items-center space-x-4 rounded-lg bg-white p-4 shadow">
+				<div class="text-3xl text-green-600">✉</div>
+				<div>
+					<h3 class="sm:text-[16px ] mb-3 w-full w-full text-2xl text-sky-800">
+						Working office address:
+					</h3>
+					<p class="mt-4 text-lg text-neutral-500 sm:text-base md:text-[20px]">
+						511, World Trade Centre, Connaught Place, New Delhi - 110001
+					</p>
+				</div>
+			</div>
+
+			<div class="flex items-center space-x-4 rounded-lg bg-white p-4 shadow">
+				<div class="text-3xl text-green-600">📧</div>
+				<div>
+					<h3 class="sm:text-[16px ] mb-3 w-full w-full text-2xl text-sky-800">Email address:</h3>
+					<p class="mt-4 text-lg text-neutral-500 sm:text-base md:text-[20px]">
+						secretary@cma-india.in
+					</p>
+				</div>
+			</div>
+
+			<div class="flex items-center space-x-4 rounded-lg bg-white p-4 shadow">
+				<div class="text-3xl text-green-600">📞</div>
+				<div>
+					<h3 class="sm:text-[16px ] mb-3 w-full w-full text-2xl text-sky-800">Mobile Number:</h3>
+					<p class="mt-4 text-lg text-neutral-500 sm:text-base md:text-[20px]">+91 9811779580</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="rounded-lg bg-gray-100 py-18 px-12 text-white shadow-lg">
+		<form class="space-y-4">
+			<div class="grid grid-cols-2 gap-4">
+				<input
+					type="text"
+					placeholder="Name"
+					class="w-full rounded border border-gray-200 bg-white p-3 focus:outline-none"
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					class="w-full rounded border border-gray-200 bg-white p-3 focus:outline-none"
+				/>
+			</div>
+			<div class="grid grid-cols-2 gap-4">
+				<input
+					type="text"
+					placeholder="Subject"
+					class="w-full rounded border border-gray-200 bg-white p-3 focus:outline-none"
+				/>
+				<input
+					type="text"
+					placeholder="Contact Number"
+					class="w-full rounded border border-gray-200 bg-white p-3 focus:outline-none"
+				/>
+			</div>
+			<textarea
+				placeholder="Message"
+				class="h-32 w-full rounded border border-gray-200 bg-white p-3 focus:outline-none"
+			></textarea>
+			<button
+				type="submit"
+				class="border-text-secondary w-4xs border-orange-600 bg-orange-600 px-6 py-2 text-lg font-semibold text-white transition-all duration-300 ease-in-out hover:border-orange-700 hover:bg-orange-700 hover:text-white hover:no-underline">Submit Now</button
+			>
+		</form>
+	</div>
+</section>
---
+++ src/routes/contact-us/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import ContactUsBanner from '../../components/contactus/ContactUsBanner.svelte';
+	import ContactUs from '../../components/contactus/ContactUs.svelte';
+</script>
+
+<section class="relative bg-[url('./images/banner.jpg')] bg-cover bg-center">
+	<div
+		class="absolute inset-0 z-0 bg-black opacity-50 transition-opacity duration-300 group-hover:opacity-50"
+	></div>
+	<Navbar />
+	<ContactUsBanner />
+</section>
+<ContactUs />
+
+<Footer />
