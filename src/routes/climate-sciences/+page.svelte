31a1187f82deeedb49a2859f67749fb97d4b3739
Vishal-Ravan <vksingh2992000@gmail.com>
2025-03-10 13:48:59 +0530

    chang bg image routes

--- src/routes/climate-sciences/+page.svelte
+++ src/routes/climate-sciences/+page.svelte
@@ -5,7 +5,7 @@
 	import ClimateSciences from '../../components/climatesciences/ClimateSciences.svelte';
 </script>
 
-<section class="relative bg-[url('./images/banner.jpg')] bg-cover bg-center">
+<section class="relative bg-[url('/images/banner.jpg')] bg-cover bg-center">
 	<div
 		class="absolute inset-0 z-0 bg-black opacity-50 transition-opacity duration-300 group-hover:opacity-50"
 	></div>
